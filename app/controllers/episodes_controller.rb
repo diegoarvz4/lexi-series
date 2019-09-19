38ceafbfbe27a3e76024b999e957a9b3b2ac819c
diegoarvz4 <darvz.gtz@gmail.com>
2019-09-19 17:40:45 -0500

    episode update implemented

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -6,7 +6,7 @@ class EpisodesController < ApplicationController
 
   def create
     @series = Series.find_by(id: params[:episode][:series_id])
-    episode = @series.episodes.build(number: params[:episode][:number],resume: params[:episode][:resume])
+    episode = @series.episodes.build(episode_params)
     if episode.save 
       redirect_to episode
     else
@@ -15,16 +15,31 @@ class EpisodesController < ApplicationController
   end
 
   def update
+    @episode = Episode.find_by(id: params[:id])
+    @episode.assign_attributes(episode_params)
+    if @episode.save 
+      flash[:success] = "Episode #{@episode.number} Updated!"
+      redirect_to @episode
+    else
+      render 'new'
+    end
   end
 
   def destroy
   end
 
   def edit
+    @episode = Episode.find_by(id:params[:id])
   end
 
   def show
     @episode = Episode.find_by(id: params[:id])
     @series = @episode.series
   end
+
+  private 
+
+  def episode_params
+    params.require(:episode).permit(:number, :resume)
+  end 
 end
