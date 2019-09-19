36c23ca83c15087911fadf625a19433d67900857
diegoarvz4 <darvz.gtz@gmail.com>
2019-09-19 10:58:39 -0500

    series index styling

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,7 +2,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
+  # process resize_to_limit: [400, 400]
 
 
   # Choose what kind of storage to use for this uploader:
