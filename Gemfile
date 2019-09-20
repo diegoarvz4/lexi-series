aa08c8fce411e2423ed6fcd55e1e9bf7e5722925
diegoarvz4 <darvz.gtz@gmail.com>
2019-09-20 18:24:25 -0500

    devise

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'rack-cors'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'carrierwave',             '1.2.2'
 gem 'mini_magick',             '4.7.0'
-
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :series
   resources :episodes
   resources :characters
   resources :relationships
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret'}
 end
