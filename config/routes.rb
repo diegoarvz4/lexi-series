5e1cdb293c5130d417ca51a25e81f014c0daf864
diegoarvz4 <darvz.gtz@gmail.com>
2019-09-20 19:14:46 -0500

    devise users simple signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  authenticated :user do
+    root to: 'pages#home'
+  end
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :series
   resources :episodes
   resources :characters
   resources :relationships
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret'}
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret'}
 end
