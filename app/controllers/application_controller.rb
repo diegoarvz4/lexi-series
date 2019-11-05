802989a8e47dc72f386bffec200c03d0de8f9c01
diegoarvz4 <darvz.gtz@gmail.com>
2019-11-05 15:48:10 -0600

    Added Bio update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:username,
+               :bio,
                :avatar,
                :email,
                :password,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_153543) do
+ActiveRecord::Schema.define(version: 2019_11_05_213421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_153543) do
     t.string "username"
     t.boolean "admin"
     t.string "avatar"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
