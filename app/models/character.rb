a49656b198415806216f10c6201efca3f37e0b61
diegoarvz4 <darvz.gtz@gmail.com>
2019-11-27 14:51:22 -0600

    Character name validation

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,7 +6,7 @@ class Character < ApplicationRecord
   has_many :inverse_relationships,
            class_name: 'Relationship',
            foreign_key: 'related_id', dependent: :delete_all
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
   has_many :character_images, dependent: :delete_all
   has_and_belongs_to_many :character_tags
 end
---
+++ db/migrate/20191127202610_add_creator_references.rb
@@ -0,0 +1,4 @@
+class AddCreatorReferences < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
