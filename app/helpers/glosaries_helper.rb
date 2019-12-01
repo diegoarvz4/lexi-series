12c9286bc019b9701a55e49471c80ee3595128c7
diegoarvz4 <darvz.gtz@gmail.com>
2019-12-01 13:22:17 -0600

    Term validation unique and Character Show modification

--- app/helpers/glosaries_helper.rb
+++ app/helpers/glosaries_helper.rb
@@ -1,2 +1,7 @@
 module GlosariesHelper
+
+  def sorted_terms
+    @terms.sort_by{|term| term.dst_term.downcase}
+  end
+
 end
--- app/models/term.rb
+++ app/models/term.rb
@@ -1,5 +1,5 @@
 class Term < ApplicationRecord
   belongs_to :glosary
-  validates :src_term, presence: true
+  validates :src_term, presence: true, uniqueness: { scope: :glosary_id }
   validates :dst_term, presence: true
 end
